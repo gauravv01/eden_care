4ccb6ea5a6c5aca6fa4b6638d9180d17a3e9e2e8
Gaurav Verma <vermagaurav171995@gmail.com>
2023-08-07 10:35:00 +0530

    restore

--- dist/Login.js
+++ dist/Login.js
@@ -4,11 +4,7 @@ import { Button, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 // import loginPic from '../../img/loginPic.png';
 
-const Login = ({
-  onFinish,
-  validateEmail,
-  loginPic
-}) => {
+const Login = ({ onFinish, validateEmail, loginPic }) => {
   //   const navigate = useNavigate();
   const [form] = Form.useForm();
   const [error, setError] = useState();
@@ -23,74 +19,138 @@ const Login = ({
   //     window.localStorage.setItem('refresh', '1');
   //   }
 
-  return /*#__PURE__*/React.createElement("div", {
-    className: styles.container
-  }, /*#__PURE__*/React.createElement("img", {
-    src: loginPic,
-    alt: "",
-    className: styles.loginPic
-  }), /*#__PURE__*/React.createElement("div", {
-    className: styles.box
-  }, /*#__PURE__*/React.createElement("img", {
-    src: "icons/logo.png",
-    className: styles.logo
-  }), /*#__PURE__*/React.createElement(Form, {
-    form: form,
-    className: "wFull",
-    style: {
-      padding: "0px 30px"
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    {
+      className: styles.container,
     },
-    name: "basic",
-    onFinish: onFinish
-  }, /*#__PURE__*/React.createElement("p", {
-    className: styles.fieldLabel
-  }, "Email"), /*#__PURE__*/React.createElement(Form.Item, {
-    name: "email",
-    validateTrigger: "onSubmit",
-    rules: [{
-      required: true,
-      message: "Please input your Email!"
-    }, {
-      validator: validateEmail
-    }]
-  }, /*#__PURE__*/React.createElement(Input, {
-    placeholder: "Enter your Email",
-    className: "customAntInput",
-    size: "large"
-  })), /*#__PURE__*/React.createElement("p", {
-    className: styles.fieldLabel
-  }, "Password"), /*#__PURE__*/React.createElement(Form.Item, {
-    className: "mbZero",
-    name: "password",
-    rules: [{
-      required: true,
-      message: "Please input your password!"
-    }]
-  }, /*#__PURE__*/React.createElement(Input.Password, {
-    placeholder: "Enter your Password",
-    className: "customAntInput",
-    size: "large"
-  })), /*#__PURE__*/React.createElement(Link, {
-    to: "/reset-password-request",
-    className: styles.forgottPassword
-  }, "Forgot Password")), passwordLength && /*#__PURE__*/React.createElement("span", {
-    className: styles.errorText
-  }, "Minimum 6 characters required"), error && /*#__PURE__*/React.createElement("span", {
-    className: styles.errorText
-  }, error), /*#__PURE__*/React.createElement("div", {
-    className: styles.submitBtn
-  }, /*#__PURE__*/React.createElement(Button, {
-    loading: loading,
-    onClick: () => form.submit(),
-    shape: "round",
-    style: {
-      width: "85%",
-      backgroundColor: "#3ab44d",
-      color: "white"
-    },
-    size: "large",
-    type: "text",
-    htmlType: "submit"
-  }, "Login"))));
+    /*#__PURE__*/ React.createElement("img", {
+      src: loginPic,
+      alt: "",
+      className: styles.loginPic,
+    }),
+    /*#__PURE__*/ React.createElement(
+      "div",
+      {
+        className: styles.box,
+      },
+      /*#__PURE__*/ React.createElement("img", {
+        src: "icons/logo.png",
+        className: styles.logo,
+      }),
+      /*#__PURE__*/ React.createElement(
+        Form,
+        {
+          form: form,
+          className: "wFull",
+          style: {
+            padding: "0px 30px",
+          },
+          name: "basic",
+          onFinish: onFinish,
+        },
+        /*#__PURE__*/ React.createElement(
+          "p",
+          {
+            className: styles.fieldLabel,
+          },
+          "Email"
+        ),
+        /*#__PURE__*/ React.createElement(
+          Form.Item,
+          {
+            name: "email",
+            validateTrigger: "onSubmit",
+            rules: [
+              {
+                required: true,
+                message: "Please input your Email!",
+              },
+              {
+                validator: validateEmail,
+              },
+            ],
+          },
+          /*#__PURE__*/ React.createElement(Input, {
+            placeholder: "Enter your Email",
+            className: "customAntInput",
+            size: "large",
+          })
+        ),
+        /*#__PURE__*/ React.createElement(
+          "p",
+          {
+            className: styles.fieldLabel,
+          },
+          "Password"
+        ),
+        /*#__PURE__*/ React.createElement(
+          Form.Item,
+          {
+            className: "mbZero",
+            name: "password",
+            rules: [
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ],
+          },
+          /*#__PURE__*/ React.createElement(Input.Password, {
+            placeholder: "Enter your Password",
+            className: "customAntInput",
+            size: "large",
+          })
+        ),
+        /*#__PURE__*/ React.createElement(
+          Link,
+          {
+            to: "/reset-password-request",
+            className: styles.forgottPassword,
+          },
+          "Forgot Password"
+        )
+      ),
+      passwordLength &&
+        /*#__PURE__*/ React.createElement(
+          "span",
+          {
+            className: styles.errorText,
+          },
+          "Minimum 6 characters required"
+        ),
+      error &&
+        /*#__PURE__*/ React.createElement(
+          "span",
+          {
+            className: styles.errorText,
+          },
+          error
+        ),
+      /*#__PURE__*/ React.createElement(
+        "div",
+        {
+          className: styles.submitBtn,
+        },
+        /*#__PURE__*/ React.createElement(
+          Button,
+          {
+            loading: loading,
+            onClick: () => form.submit(),
+            shape: "round",
+            style: {
+              width: "85%",
+              backgroundColor: "#3ab44d",
+              color: "white",
+            },
+            size: "large",
+            type: "text",
+            htmlType: "submit",
+          },
+          "Login"
+        )
+      )
+    )
+  );
 };
-export default Login;
+export { Login };
--- dist/Welcome.js
+++ dist/Welcome.js
@@ -5,104 +5,185 @@ import { Button, Divider, Form, Input } from "antd";
 
 // import ResetPasswordScreen from "./ResetPasswordScreen";
 
-const Welcome = ({
-  loginPic,
-  onFinish
-}) => {
+const Welcome = ({ loginPic, onFinish }) => {
   const [form] = Form.useForm();
   const [error, setError] = useState();
   const [loading, setLoading] = useState();
   const [currentPassword, setCurrentPassword] = useState();
   const [screen, setScreen] = useState(1);
   const [passwordLength, setPasswordLength] = useState(false);
-  return /*#__PURE__*/React.createElement("div", {
-    className: styles.container
-  }, /*#__PURE__*/React.createElement("div", {
-    className: styles.contentBox
-  }, /*#__PURE__*/React.createElement("div", {
-    style: {
-      display: "flex",
-      justifyContent: "space-between"
-    }
-  }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h3", {
-    className: styles.welcomeTitle
-  }, "Welcome to Eden Care Medical"), /*#__PURE__*/React.createElement("p", {
-    className: styles.welcomeText
-  }, "Please log in to your account using the credentials that you received via email.")), /*#__PURE__*/React.createElement("img", {
-    src: loginPic,
-    alt: "",
-    className: styles.loginPic
-  })), /*#__PURE__*/React.createElement(Divider, {
-    style: {
-      margin: 0
-    }
-  }), /*#__PURE__*/React.createElement("div", {
-    style: {
-      display: "flex",
-      justifyContent: "center"
-    }
-  }, /*#__PURE__*/React.createElement("div", {
-    className: styles.loginBox
-  }, /*#__PURE__*/React.createElement(Form, {
-    form: form,
-    className: "wFull",
-    style: {
-      padding: "0px 30px"
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    {
+      className: styles.container,
     },
-    name: "basic",
-    onFinish: onFinish
-  }, /*#__PURE__*/React.createElement("p", {
-    className: styles.fieldLabel
-  }, "Email"), /*#__PURE__*/React.createElement(Form.Item, {
-    name: "email",
-    rules: [{
-      required: true,
-      message: "A valid email is required"
-    }]
-  }, /*#__PURE__*/React.createElement(Input, {
-    placeholder: "Enter your Email",
-    className: "customAntInput",
-    size: "large"
-  })), /*#__PURE__*/React.createElement("p", {
-    className: styles.fieldLabel
-  }, "Password"), /*#__PURE__*/React.createElement(Form.Item, {
-    className: "mbZero",
-    name: "password",
-    rules: [{
-      required: true,
-      message: "Password required for authentication"
-    }]
-  }, /*#__PURE__*/React.createElement(Input.Password, {
-    placeholder: "Enter your Password",
-    className: "customAntInput",
-    size: "large"
-  }))), passwordLength && /*#__PURE__*/React.createElement("span", {
-    style: {
-      color: "red",
-      marginTop: -20,
-      marginBottom: -10
-    }
-  }, "Minimum 6 characters required"), error && /*#__PURE__*/React.createElement("span", {
-    style: {
-      color: "red",
-      marginTop: -20,
-      marginBottom: -10,
-      width: "70%"
-    }
-  }, error), /*#__PURE__*/React.createElement("div", {
-    className: styles.submitBtn
-  }, /*#__PURE__*/React.createElement(Button, {
-    loading: loading,
-    onClick: () => form.submit(),
-    shape: "round",
-    style: {
-      width: "65%",
-      backgroundColor: "#3ab44d",
-      color: "white"
-    },
-    size: "large",
-    type: "text",
-    htmlType: "submit"
-  }, "Login"))))));
+    /*#__PURE__*/ React.createElement(
+      "div",
+      {
+        className: styles.contentBox,
+      },
+      /*#__PURE__*/ React.createElement(
+        "div",
+        {
+          style: {
+            display: "flex",
+            justifyContent: "space-between",
+          },
+        },
+        /*#__PURE__*/ React.createElement(
+          "div",
+          null,
+          /*#__PURE__*/ React.createElement(
+            "h3",
+            {
+              className: styles.welcomeTitle,
+            },
+            "Welcome to Eden Care Medical"
+          ),
+          /*#__PURE__*/ React.createElement(
+            "p",
+            {
+              className: styles.welcomeText,
+            },
+            "Please log in to your account using the credentials that you received via email."
+          )
+        ),
+        /*#__PURE__*/ React.createElement("img", {
+          src: loginPic,
+          alt: "",
+          className: styles.loginPic,
+        })
+      ),
+      /*#__PURE__*/ React.createElement(Divider, {
+        style: {
+          margin: 0,
+        },
+      }),
+      /*#__PURE__*/ React.createElement(
+        "div",
+        {
+          style: {
+            display: "flex",
+            justifyContent: "center",
+          },
+        },
+        /*#__PURE__*/ React.createElement(
+          "div",
+          {
+            className: styles.loginBox,
+          },
+          /*#__PURE__*/ React.createElement(
+            Form,
+            {
+              form: form,
+              className: "wFull",
+              style: {
+                padding: "0px 30px",
+              },
+              name: "basic",
+              onFinish: onFinish,
+            },
+            /*#__PURE__*/ React.createElement(
+              "p",
+              {
+                className: styles.fieldLabel,
+              },
+              "Email"
+            ),
+            /*#__PURE__*/ React.createElement(
+              Form.Item,
+              {
+                name: "email",
+                rules: [
+                  {
+                    required: true,
+                    message: "A valid email is required",
+                  },
+                ],
+              },
+              /*#__PURE__*/ React.createElement(Input, {
+                placeholder: "Enter your Email",
+                className: "customAntInput",
+                size: "large",
+              })
+            ),
+            /*#__PURE__*/ React.createElement(
+              "p",
+              {
+                className: styles.fieldLabel,
+              },
+              "Password"
+            ),
+            /*#__PURE__*/ React.createElement(
+              Form.Item,
+              {
+                className: "mbZero",
+                name: "password",
+                rules: [
+                  {
+                    required: true,
+                    message: "Password required for authentication",
+                  },
+                ],
+              },
+              /*#__PURE__*/ React.createElement(Input.Password, {
+                placeholder: "Enter your Password",
+                className: "customAntInput",
+                size: "large",
+              })
+            )
+          ),
+          passwordLength &&
+            /*#__PURE__*/ React.createElement(
+              "span",
+              {
+                style: {
+                  color: "red",
+                  marginTop: -20,
+                  marginBottom: -10,
+                },
+              },
+              "Minimum 6 characters required"
+            ),
+          error &&
+            /*#__PURE__*/ React.createElement(
+              "span",
+              {
+                style: {
+                  color: "red",
+                  marginTop: -20,
+                  marginBottom: -10,
+                  width: "70%",
+                },
+              },
+              error
+            ),
+          /*#__PURE__*/ React.createElement(
+            "div",
+            {
+              className: styles.submitBtn,
+            },
+            /*#__PURE__*/ React.createElement(
+              Button,
+              {
+                loading: loading,
+                onClick: () => form.submit(),
+                shape: "round",
+                style: {
+                  width: "65%",
+                  backgroundColor: "#3ab44d",
+                  color: "white",
+                },
+                size: "large",
+                type: "text",
+                htmlType: "submit",
+              },
+              "Login"
+            )
+          )
+        )
+      )
+    )
+  );
 };
-export default Welcome;
+export { Welcome };
