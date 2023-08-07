d15eb644c188f9f5f8ad5411d6568d1c94c9294e
Gaurav Verma <vermagaurav171995@gmail.com>
2023-08-07 10:39:50 +0530

    restore

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // import Login from "./dist/Login";
 // import Welcome from "./dist/Welcome";
 
-export { default as LoginComponent } from "./dist/Login";
-export { default as Welcome } from "./dist/Welcome";
+// export { default as LoginComponent } from "./dist/Login";
+// export { default as Welcome } from "./dist/Welcome";
+
+// src/index.js
+export { default as Component1 } from "./dist/Login";
+export { default as Component2 } from "./dist/Welcome";
