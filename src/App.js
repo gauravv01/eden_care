5e16ab71b60c36e802464848ec3642a3fbadac65
Gaurav Verma <vermagaurav171995@gmail.com>
2023-08-07 08:17:17 +0530

    library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      A paragraph is a series of sentences that are organized and coherent, and
+      are all related to a single topic. Almost every piece of writing you do
+      that is longer than a few sentences should be organized into paragraphs.
     </div>
   );
 }
